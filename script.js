ff50ae015881579fcdb9c8c34967e65a6f0c2e6c
younglord3302 <shankhadeepmondal7@gmail.com>
2024-11-18 02:36:27 +0530

    updates

---
+++ script.js
@@ -0,0 +1,20 @@
+// var varibleName = "Welcome to veriables.!";
+
+// console.log(varibleName);
+
+// const age = 18;
+// if (age > 18) {
+//   console.log("You are an adult.");
+// } else if (age === 18) {
+//   console.log("You are almost an adult.");
+// } else {
+//   console.log("You are a minor.");
+// }
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
